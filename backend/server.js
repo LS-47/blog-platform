25a7e75e2608d3319261a2c2fc6559b85ca8fbc7
Sumit Deverkonda <sumit758030@gmail.com>
2026-07-06 15:18:48 +0000

    Connect Express server to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Server is running");
