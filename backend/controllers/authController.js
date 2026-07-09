f62a76cb2f170661a7cdbdfb4600a6025b573688
Sumit Deverkonda <sumit758030@gmail.com>
2026-07-09 19:18:42 +0000

    Add JWT authentication middleware

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -46,6 +46,50 @@ const registerUser = async (req, res) => {
     }
 };
 
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Check fields
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Please enter email and password",
+            });
+        }
+
+        // Find user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password",
+            });
+        }
+
+        // Return token
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
     registerUser,
+    loginUser,
 };
